2159f779a1069d6b61dc7eaf8d2ce995deb87b65
himanshu82817 <himanshu82817@gmail.com>
2023-05-28 12:36:33 +0530

    components added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import {BrowserRouter, Routes, Route }  from 'react-router-dom'
+import Login from "./pages/Login"
+import Chat from "./pages/Chat"
+import Register from "./pages/Register"
 
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold text-red-500 underline text-center">Hello world!</h1> 
+     <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register/>} />
+        <Route path="chat" element={<Chat/>} />
+      </Routes>
+     </BrowserRouter>
   )
 }
 
